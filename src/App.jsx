b5aa30dd7d6dc368c4727e616b6da458479779bf
Project-Management <vedang.prajapati18@gmail.com>
2024-03-14 23:34:51 +0530

    Updates Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import "./App.css";
-
 function App() {
   return (
     <>
-      <h1>Welcome to React Project</h1>
+      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
     </>
   );
 }
