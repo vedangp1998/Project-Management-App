e5c27a070086a2ad12cf97359d8b07e9614a0e33
Project-Management <vedang.prajapati18@gmail.com>
2024-03-23 02:44:18 +0530

    Adding addtask and clear task

--- src/App.jsx
+++ src/App.jsx
@@ -9,25 +9,42 @@ function App() {
   const [projectsState, setProjectsState] = useState({
     selectedProjectId: undefined,
     projects: [],
+    tasks: [],
   });
 
-  const handleSelectProject = (id) => {
+  function handleAddTask(text) {
+    setProjectsState((prevState) => {
+      const taskId = Math.random();
+      const newTask = {
+        text: text,
+        projectId: prevState.selectedProjectId,
+        id: taskId,
+      };
+
+      return {
+        ...prevState,
+        tasks: [newTask, ...prevState.tasks],
+      };
+    });
+  }
+
+  function handleDeleteTask(id) {
     setProjectsState((prevState) => {
       return {
         ...prevState,
-        selectedProjectId: id,
+        tasks: prevState.tasks.filter((task) => task.id !== id),
       };
     });
-  };
+  }
 
-  function handleStartAddProject() {
+  const handleStartAddProject = () => {
     setProjectsState((prevState) => {
       return {
         ...prevState,
         selectedProjectId: null,
       };
     });
-  }
+  };
 
   const handleCanceProject = () => {
     setProjectsState((prevState) => {
@@ -53,11 +70,40 @@ function App() {
     });
   };
 
+  const handleSelectProject = (id) => {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: id,
+      };
+    });
+  };
+
+  const handleDeleteProject = () => {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter((project) => {
+          project.id !== prevState.selectedProjectId;
+        }),
+      };
+    });
+  };
+
   const selectedProject = projectsState.projects.find(
     (project) => project.id === projectsState.selectedProjectId
   );
 
-  let content = <SelectedProject project={selectedProject} />;
+  let content = (
+    <SelectedProject
+      project={selectedProject}
+      onDelete={handleDeleteProject}
+      onAddTask={handleAddTask}
+      onDeleteTask={handleDeleteTask}
+      tasks={projectsState.tasks}
+    />
+  );
 
   if (projectsState.selectedProjectId === null) {
     content = (
